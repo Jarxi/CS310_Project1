e2e804024faf4358ce6d59d2ebc977f97b0a8b6f
Ruoxi Jia <ruoxijia@usc.edu>
2018-09-11 19:40:47 -0700

    finish reading map

--- src/main/FlightMap.java
+++ src/main/FlightMap.java
@@ -1,11 +1,70 @@
 package main;
 
+
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class FlightMap {
-    public static void main(String args[]){
+    //store origin
+    private String origin;
+    //store origin,destination and distance
+    private HashMap<String, HashMap<String, Integer>> adj; //adjacent nodes list
 
+    public HashMap<String, HashMap<String, Integer>> getAdj() {
+        return adj;
     }
 
-    public void test(){
-
+    /**
+     * add edge between origin and destination with distance
+     * @param o origin
+     * @param d destination
+     * @param distance
+     */
+    private void addEdge(String o, String d, Integer distance){
+        if(adj.containsKey(o)){
+            adj.get(o).put(d,distance);
+        }else{
+            HashMap<String, Integer> temp = new HashMap<>();
+            temp.put(d, distance);
+            adj.put(o,temp);
+        }
     }
+
+
+    private void createMap(String filename) throws FileNotFoundException {
+        System.out.println(new File(".").getAbsoluteFile());
+        //read in file
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            //read line by line
+            while ((line = br.readLine()) != null) {
+                String[] stringArray=line.split(" ");
+                if (stringArray.length<3 && stringArray!=null){
+                    this.origin = stringArray[0].toLowerCase();
+                    continue;
+                }
+                //create mapping
+                if (stringArray.length == 3){
+                    for (int i=0; i<stringArray.length; i++){
+                        Integer dist = Integer.parseInt(stringArray[2]);
+                        addEdge(stringArray[0].toLowerCase(),
+                                stringArray[1].toLowerCase(), dist);
+                    }
+                }
+            }
+        } catch (FileNotFoundException fileNotFound){
+            fileNotFound.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //create new flightMap
+    public FlightMap(String filename) throws FileNotFoundException {
+        adj = new HashMap<>();
+        createMap(filename);
+    }
+
 }
--- src/main/SearchMap.java
+++ src/main/SearchMap.java
@@ -1,12 +1,58 @@
 package main;
 
-public class SearchMap {
-    public static void main(String [] args) {
-        System.out.println("searchMap");
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import static java.util.Collections.addAll;
+
+public class SearchMap {
+
+    private FlightMap map;
+
+
+    public FlightMap getMap() {
+        return map;
     }
 
-    public void test(){
+    public void setMap(FlightMap map) {
+        this.map = map;
+    }
 
+    /**
+     * put all cities in a map and give the boolean value to represent
+     * visited or not
+     * @param adj
+     * @return HashMap<String, Boolean>
+     */
+    public HashMap<String, Boolean> mergeCity(HashMap<String, HashMap<String, Integer>> adj){
+        Set<String> citySet1 = adj.keySet();
+        HashMap<String, Boolean> visited = new HashMap<>();
+        for (String key : citySet1){
+            visited.put(key,false);
+            for (String key2 : adj.get(key).keySet())
+            visited.put(key2, false);
+        }
+        return visited;
+    }
+
+    /**
+     * Search through the map and giving the output
+     * @param map
+     */
+    public SearchMap(FlightMap map){
+        this.map = map;
+        HashMap<String, HashMap<String, Integer>> adj = map.getAdj();
+        HashMap<String, Boolean> visited = mergeCity(adj);
+    }
+    public static void main(String [] args) throws FileNotFoundException {
+        if (args.length<2){
+            System.out.println("Please enter two filenames.");
+        }else{
+            FlightMap flightMap = new FlightMap(args[0]);
+            SearchMap searchMap = new SearchMap(flightMap);
+        }
     }
 }
